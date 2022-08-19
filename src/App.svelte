b29e19b8c2b5d4a7007006f42d5932023ebb4235
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-08-19 01:48:53 +0100

    updated App component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,57 @@
 <script>
-	export let name;
+import Loader from "./Loader.svelte";
+import WordData from "./WordData.svelte";
+
+let word = "";
+let loading = false;
+let wordData = null;
+
+const searchWord = async () => {
+	if(word.length === 0) return;
+
+	loading = true;
+	wordData = null;
+	try {
+		let res = await fetch("https://api.dictionaryapi.dev/api/v2/entries/en/"+word);
+		const data = await res.json()
+		if(Array.isArray(data)) wordData = data[0]
+		else wordData = "No result"
+
+		loading = false;
+	} catch (error) {
+		loading = false;
+	}
+}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div class="header">Dictionary App</div>
+
+	<div class="center">
+		<div class="form">
+			<div class="form-group">
+				<span>Search Word</span>
+				<input type="text" placeholder="Type word here" bind:value={word} />
+			</div>
+			<div class="form-group">
+				<button on:click={searchWord}>Search</button>
+			</div>
+		</div>
+
+		{#if loading === true || wordData !== null}
+		<div class="result">
+			{#if wordData !== null && typeof wordData !== 'string'}
+			<WordData wordData={wordData} />
+			{:else if wordData === null && loading}
+			<Loader />
+			{:else}
+			<p class="padding">No result found</p>
+			{/if}
+		</div>
+		{/if}
+	</div>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+<style lang="scss">
+	@import './styles/app.styles.scss';
 </style>
